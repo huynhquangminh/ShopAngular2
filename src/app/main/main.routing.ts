20dc246479f86f8a6f599eb1e9fa3481aa4c4401
DESKTOP-IJFJJCG\MINH <huynhtony08@gmail.com>
2018-04-04 20:38:28 +0700

    create layout main

--- src/app/main/main.routing.ts
+++ src/app/main/main.routing.ts
@@ -4,6 +4,7 @@ import { Routes } from '@angular/router';
 export const mainRoutes: Routes = [
     {path : '', component : MainComponent, children : [
         { path: '', redirectTo: 'home', pathMatch: 'full' },
+        {path : 'home', loadChildren: './home/home.module#HomeModule'},
         {path : 'user', loadChildren: './user/user.module#UserModule'}
     ]}
 ];
